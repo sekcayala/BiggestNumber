b42897ce48ca0362205e7e4e55c3462f32e70929
sekcayala <aayala39@mail.csuchico.edu>
2018-02-05 19:48:42 -0800

    big number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -6,11 +6,11 @@ using namespace std; //so we don't need the prefix std::
 int main()
 {
   int number1;
-  cout<<"Please enter a whole number: \n";
+  cout<<"Please enter a whole number:\n";
   cin >>number1;
   
   int number2;
-  cout<<"Please enter another whole number: \n";
+  cout<<"Please enter another whole number:\n";
   cin >>number2;
 
   int biggest;
@@ -23,7 +23,7 @@ int main()
   {
   biggest = number2;
   }
-  cout<<"Of those two numbers, the biggest is: " <<biggest<<" \n";
+  cout<<"Of those two numbers, the biggest is: "<<biggest<<"\n";
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
