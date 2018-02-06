f900d709cd1bd45c90deb03deab9266c2bd82202
sekcayala <aayala39@mail.csuchico.edu>
2018-02-05 18:18:41 -0800

    number bigger than other

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,16 +1,30 @@
-//Author:
+//Author:Alejandro Ayala 
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
 
 int main()
 {
+  int number1;
+  cout<<"Please enter a whole number: \n";
+  cin >>number1;
   
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+  int number2;
+  cout<<"Please enter another whole number: \n";
+  cin >>number2;
 
-  cout<<"Of those two numbers, the biggest is: ";
+  int biggest;
+  
+  if (number1 >  number2) 
+  {
+  biggest = number1;
+  }
+  else 
+  {
+  biggest = number2;
+  }
+  cout<<"Of those two numbers, the biggest is: \n";
+  std::cout <<biggest;
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
